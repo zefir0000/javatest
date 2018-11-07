d95924519d5a27d7a1465f0dc3438a0da2e5711d
G2A\slawomir.lenczner <slenczner@g2a.com>
2018-11-07 14:14:19 +0100

    no message

---
+++ src/test/java/testweb.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class testweb {
+
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void createAndStartService() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("driver/chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @AfterAll
+    public static void createAndStopService() {
+        service.stop();
+    }
+
+    @BeforeEach
+    public void createDriver() {
+        driver = new RemoteWebDriver(service.getUrl(),
+                new ChromeOptions());
+    }
+
+    @AfterEach
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://cryptovoucher.io/buy-voucher");
+        WebElement logo = driver.findElement(By.className("NavigationBar__logo"));
+        logo.click();
+
+        assertEquals("Bitcoin as gift card? Buy BTC online instantly with credit card", driver.getTitle());
+        assertEquals("https://cryptovoucher.io/", driver.getCurrentUrl());
+
+        driver.quit();
+    }
+}
